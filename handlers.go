e0ca8fc0f4b22d50d8806a752490939ba779565b
Afsinoz <afsinozdemir@gmail.com>
2025-02-26 17:03:35 +0100

    chirpy text validation added, api and app logic seperated

--- handlers.go
+++ handlers.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,5 +15,84 @@ func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	w.Write([]byte("OK, ready!"))
+
+}
+
+// API CONFIG
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) RequestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("contentType", "text/html")
+	w.WriteHeader(http.StatusOK)
+	s := fmt.Sprintf("<html> <body> <h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	w.Write([]byte(s))
+}
+
+func (cfg *apiConfig) ResetNumberRequestHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("contentType", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("All resetted!"))
+}
+
+func ChirpyValidationHandler(w http.ResponseWriter, r *http.Request) {
+
+	type chirpyText struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := chirpyText{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	type ResponseBody struct {
+		Valid bool   `json:"valid"`
+		Err   string `json:"error"`
+	}
+
+	if len(params.Body) <= 140 {
+		vld := ResponseBody{
+			Valid: true,
+			Err:   "",
+		}
+
+		dat, err := json.Marshal(vld)
+		if err != nil {
+			log.Printf("Error Marshalling Json: %s", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(dat)
+
+	} else {
+		vld := ResponseBody{
+			Valid: false,
+			Err:   "Chirp is too long",
+		}
+		dat, err := json.Marshal(vld)
+		if err != nil {
+			log.Printf("Error Marshalling Json: %s", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+	}
 
 }
--- main.go
+++ main.go
@@ -3,51 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync/atomic"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) NumberRequestHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("contentType", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	s := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
-	w.Write([]byte(s))
-}
-
-func (cfg *apiConfig) ResetNumberRequestHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("contentType", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("All resetted!"))
-}
 
 func main() {
 	const port = "8080"
+	const filePathRoot = "./templates"
 	var apiCfg apiConfig
 	mux := http.NewServeMux()
 	// Handler
-	fs := http.FileServer(http.Dir("."))
 
-	strpr := http.StripPrefix("/app/", fs)
+	strpr := http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot)))
 
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(strpr))
+	mux.Handle("/app/", apiCfg.MiddlewareMetricsInc(strpr))
 	// Registering handlers
 
-	mux.HandleFunc("GET /healthz", ReadinessHandler)
+	mux.HandleFunc("GET /api/healthz", ReadinessHandler)
 
-	mux.HandleFunc("GET /metrics", apiCfg.NumberRequestHandler)
-	mux.HandleFunc("POST /reset", apiCfg.ResetNumberRequestHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.RequestHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.ResetNumberRequestHandler)
+
+	mux.HandleFunc("POST /api/validate_chirp", ChirpyValidationHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
